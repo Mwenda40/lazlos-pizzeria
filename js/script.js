ebed5ed10b9ba92b7ef760c4590f5d2a4be7b3f9
mwenda40 <iankirimi@yahoo.com>
2021-08-02 17:21:53 +0300

    add toggle function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
-    function deliveryPrompt(){
-        $('#btnhide').hide();
-        $('#btnunhide').click(function(){
-            $('#btnhide').toggle();
-        });
+    // function deliveryPrompt(){
+    //     $('#btnhide').hide();
+    //     $('#btnunhide').click(function(){
+    //         $('#btnhide').toggle();
+    //     });
+    // }
+    // $("#btnunhide").click(function(){
+    //     // $("#btnunhide").toggle();
+    //     $("#btnhide").toggle();
+    // });
+    $("#btnhide").hide();
+    function(){
+        btnunhide.innerHTML = 
     }
+//     $("#btnunhide").click(function(){
+//         $("#btnhide").toggle();
+//   });
+
+
 
 });
