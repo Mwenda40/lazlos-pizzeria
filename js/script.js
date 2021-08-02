e188064d4e43c9051a7ba505f9fc53864ba70ea6
mwenda40 <iankirimi@yahoo.com>
2021-08-02 13:30:35 +0300

    add javascript toggle

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    function deliveryPrompt(){
+        $('#btnhide').hide();
+        $('#btnunhide').click(function(){
+            $('#btnhide').toggle();
+        });
+    }
+
+});
