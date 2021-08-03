3885d4b60238332d41491759ff191b5ef9b4283f
mwenda40 <iankirimi@yahoo.com>
2021-08-03 03:48:26 +0300

    changed image path

--- js/script.js
+++ js/script.js
@@ -1,21 +1,8 @@
 $(document).ready(function(){
-    // function deliveryPrompt(){
-    //     $('#btnhide').hide();
-    //     $('#btnunhide').click(function(){
-    //         $('#btnhide').toggle();
-    //     });
-    // }
-    // $("#btnunhide").click(function(){
-    //     // $("#btnunhide").toggle();
-    //     $("#btnhide").toggle();
-    // });
     $("#btnhide").hide();
-    function(){
-        btnunhide.innerHTML = 
-    }
-//     $("#btnunhide").click(function(){
-//         $("#btnhide").toggle();
-//   });
+    $("#btnunhide").click(function(){
+        $("#btnhide").toggle();
+  });
 
 
 
