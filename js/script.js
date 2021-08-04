97a3cd4b14d9625ae65fa8999e2cac1ea652d3a2
mwenda40 <iankirimi@yahoo.com>
2021-08-04 06:36:25 +0300

    add javascript functions and edit html form

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
+  
+    
     $("#btnhide").hide();
     $("#btnunhide").click(function(){
         $("#btnhide").toggle();
-  });
+    });
+
+    document.getElementById("done").addEventListener("click", location);
+    function location(){
+      address = document.getElementById("location").value;
+      document.getElementById("btnhide").innerHTML = "Your order will be delivered to: " + address;
+    }
 
 
+    document.getElementById("chekout").addEventListener("click", checkout);
+    function checkout(){
 
+      var total = 0;
+      var inputs = document.querySelectorAll('input');
+    
+      inputs.forEach(input => {
+        if (input.checked)
+          total += parseInt(input.value);
+      })
+    
+
+      document.getElementById("results").innerHTML = "Total: Kshs "  + total ;
+    
+    }
 });
